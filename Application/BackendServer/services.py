71c7c4e736aee72429721adaeff49bbfcf98b8a0
Sakuni99 <sakunihiru@gmail.com>
2023-12-10 14:46:43 +0530

    add functions of creating sectrograms and stacking them.

---
+++ Application/BackendServer/services.py
@@ -0,0 +1,29 @@
+from flask import Flask, request, jsonify
+from Application.BackendServer.spectrogram import createSpectrogram
+import random
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    print("hello")
+    return "hello"
+
+
+@app.route('/giveResult', methods=['GET'])
+def giveResult():
+    print("hiiiii")
+    audio = request.files['audio']
+    createSpectrogram(audio)
+    random_value = random.choice([0, 1])
+    if random_value == 0:
+        result_data = {'result': False, 'diseases': {}}
+        return jsonify(result_data)
+    else:
+        result_data = {'result': True, 'diseases': {'disease1': 0.5, 'disease2': 0.3, 'disease3': 0.2}}
+        return jsonify(result_data)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
