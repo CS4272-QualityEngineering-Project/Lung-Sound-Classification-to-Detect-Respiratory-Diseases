926ec51bea37689c5d10ba733b83683805cdaf6f
Sakuni99 <sakunihiru@gmail.com>
2024-01-01 09:57:39 +0530

    Complete the classification functions

--- Application/BackendServer/services.py
+++ Application/BackendServer/services.py
@@ -2,9 +2,17 @@ import joblib
 from flask import Flask, request, jsonify
 from Application.BackendServer.spectrogram import createSpectrogram
 import random
-import tensorflow as tf
 
 app = Flask(__name__)
+
+# model = joblib.load('tf_binary_model.h5')
+# print("model loaded")
+
+# @app.before_request()
+# def load_model():
+#     global model
+#     model = joblib.load('tf_binary_model.h5')
+#     print("model loaded")
 
 
 @app.route('/')
@@ -17,14 +25,8 @@ def index():
 def giveResult():
     print("hiiiii")
     audio = request.files['audio']
-    createSpectrogram(audio)
-    random_value = random.choice([0, 1])
-    if random_value == 0:
-        result_data = {'result': False, 'diseases': {}}
-        return jsonify(result_data)
-    else:
-        result_data = {'result': True, 'diseases': {'disease1': 0.5, 'disease2': 0.3, 'disease3': 0.2}}
-        return jsonify(result_data)
+    result = createSpectrogram(audio)
+    return result
 
 
 if __name__ == '__main__':
--- Application/BackendServer/spectrogram.py
+++ Application/BackendServer/spectrogram.py
@@ -1,8 +1,27 @@
-
 import librosa
 import librosa.display
 import matplotlib.pyplot as plt
+import numpy
 import numpy as np
+import tensorflow as tf
+from flask import jsonify
+
+from Application.BackendServer.classificationModel import do_primary_prediction, do_secondary_prediction
+
+
+def generate_mel_spec(audio):
+    mel = librosa.power_to_db(librosa.feature.melspectrogram(y=audio, sr=22050, n_mels=128, n_fft=2048, hop_length=512))
+    return mel
+
+
+def generate_mfcc(audio):
+    mfcc = librosa.feature.mfcc(y=audio, n_mfcc=128, n_fft=2048, hop_length=512)
+    return mfcc
+
+
+def generate_chroma(audio):
+    chroma = librosa.feature.chroma_stft(y=audio, sr=22050, n_chroma=128, n_fft=2048, hop_length=512)
+    return chroma
 
 
 def createSpectrogram(audio):
@@ -10,6 +29,10 @@ def createSpectrogram(audio):
 
     # Load audio file
     y, sr = librosa.load(audio, sr=22500, duration=6)
+
+    # mean = np.mean(y)
+    # std = np.std(y)
+    # norm_audio = (y - mean) / std
 
     # Extract spectrogram
     spectrogram = librosa.feature.melspectrogram(y=y, sr=sr, n_fft=2048, hop_length=512, n_mels=128)
@@ -46,8 +69,12 @@ def createSpectrogram(audio):
     plt.close()
     print("chroma.png saved")
 
-    three_chanel = np.stack((spectrogram, mfcc, chroma), axis=2)
-    plt.figure(figsize=(3, 3))
+    mel = generate_mel_spec(y)
+    mfcc_1 = generate_mfcc(y)
+    chroma_1 = generate_chroma(y)
+
+    three_chanel = np.stack((mel, mfcc_1, chroma_1), axis=2)
+    plt.figure(figsize=(4, 4))
     plt.imshow(three_chanel)
     plt.axis('off')
     plt.subplots_adjust(left=0, right=1, bottom=0, top=1)
@@ -55,3 +82,18 @@ def createSpectrogram(audio):
     plt.savefig("stacked.png")
     plt.close()
     print("stacked.png saved")
+
+    expanded_sample = tf.expand_dims(three_chanel, axis=0)
+
+    print(expanded_sample.shape)
+
+    result = do_primary_prediction(expanded_sample)
+    if not result:
+        result_data = {'result': False, 'diseases': {}}
+        return jsonify(result_data)
+    else:
+        secondary_result = do_secondary_prediction(expanded_sample)
+        for key, value in secondary_result.items():
+            if isinstance(value, np.float32):
+                secondary_result[key] = float(value)
+        return jsonify(secondary_result)
