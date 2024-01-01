275d2c5a5233df4dfc3e2f50ed8e417ed251e2e1
Sakuni99 <sakunihiru@gmail.com>
2024-01-01 10:04:20 +0530

    Refactor code

--- Application/BackendServer/spectrogram.py
+++ Application/BackendServer/spectrogram.py
@@ -34,40 +34,40 @@ def createSpectrogram(audio):
     # std = np.std(y)
     # norm_audio = (y - mean) / std
 
-    # Extract spectrogram
-    spectrogram = librosa.feature.melspectrogram(y=y, sr=sr, n_fft=2048, hop_length=512, n_mels=128)
-
-    # Plot Mel spectrogram
-    plt.figure(figsize=(3, 3))
-    librosa.display.specshow(librosa.power_to_db(spectrogram, ref=np.max), y_axis='mel', x_axis='time')
-    plt.axis('off')
-    plt.subplots_adjust(left=0, right=1, bottom=0, top=1)
-    # plt.show()
-    plt.savefig("mel_spec_plot.png", bbox_inches='tight', pad_inches=0)
-    plt.close()
-    print("mel_spec_plot.png saved")
-
-    # Extract MFCC
-    mfcc = librosa.feature.mfcc(y=y, n_mfcc=128, n_fft=2048, hop_length=512)
-    plt.figure(figsize=(3, 3))
-    librosa.display.specshow(mfcc, x_axis='time')
-    plt.axis('off')
-    plt.subplots_adjust(left=0, right=1, bottom=0, top=1)
-    # plt.show()
-    plt.savefig("mfcc_plot.png")
-    plt.close()
-    print("mfcc_plot.png saved")
-
-    # Extract Chroma
-    chroma = librosa.feature.chroma_stft(y=y, sr=22050, n_chroma=128, n_fft=2048, hop_length=512)
-    plt.figure(figsize=(3, 3))
-    librosa.display.specshow(chroma, y_axis='chroma', x_axis='time')
-    plt.axis('off')
-    plt.subplots_adjust(left=0, right=1, bottom=0, top=1)
-    # plt.show()
-    plt.savefig("chroma.png")
-    plt.close()
-    print("chroma.png saved")
+    # # Extract spectrogram
+    # spectrogram = librosa.feature.melspectrogram(y=norm_audio, sr=sr, n_fft=2048, hop_length=512, n_mels=128)
+    #
+    # # Plot Mel spectrogram
+    # plt.figure(figsize=(3, 3))
+    # librosa.display.specshow(librosa.power_to_db(spectrogram, ref=np.max), y_axis='mel', x_axis='time')
+    # plt.axis('off')
+    # plt.subplots_adjust(left=0, right=1, bottom=0, top=1)
+    # # plt.show()
+    # plt.savefig("mel_spec_plot.png", bbox_inches='tight', pad_inches=0)
+    # plt.close()
+    # print("mel_spec_plot.png saved")
+    #
+    # # Extract MFCC
+    # mfcc = librosa.feature.mfcc(y=norm_audio, n_mfcc=128, n_fft=2048, hop_length=512)
+    # plt.figure(figsize=(3, 3))
+    # librosa.display.specshow(mfcc, x_axis='time')
+    # plt.axis('off')
+    # plt.subplots_adjust(left=0, right=1, bottom=0, top=1)
+    # # plt.show()
+    # plt.savefig("mfcc_plot.png")
+    # plt.close()
+    # print("mfcc_plot.png saved")
+    #
+    # # Extract Chroma
+    # chroma = librosa.feature.chroma_stft(y=norm_audio, sr=22050, n_chroma=128, n_fft=2048, hop_length=512)
+    # plt.figure(figsize=(3, 3))
+    # librosa.display.specshow(chroma, y_axis='chroma', x_axis='time')
+    # plt.axis('off')
+    # plt.subplots_adjust(left=0, right=1, bottom=0, top=1)
+    # # plt.show()
+    # plt.savefig("chroma.png")
+    # plt.close()
+    # print("chroma.png saved")
 
     mel = generate_mel_spec(y)
     mfcc_1 = generate_mfcc(y)
